475693499b9cd8253234cb2a2228754a8127008d
David Shen <me@pantherman594.com>
2017-12-21 18:45:52 -0500

    Restructure for stow

--- sync.sh
+++ sync.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-rsync -rLptgoDv ../dotfiles_links/ .
+hash dialog 2>/dev/null || { echo >&2 "I require dialog but it's not installed.  Aborting."; exit 1; }
+hash stow 2>/dev/null || { echo >&2 "I require stow but it's not installed.  Aborting."; exit 1; }
+
+FILES=""
+
+for dir in `ls -d */`
+do
+	FILES="$FILES ${dir%%/} | on "
+done
+echo $FILES
+
+CONFIGS=`dialog --stdout --checklist "Select configurations to install" 14 48 14 $FILES`
+
+if test $? -eq 0
+then
+	clear
+	stow $CONFIGS -t ~
+	echo Configuration installation completed
+else
+	clear
+	echo Configuration installation cancelled
+fi
