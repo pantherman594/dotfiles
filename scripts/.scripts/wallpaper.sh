c93eea41e7d11d3f750c12fccb95b813d30f9d2d
David Shen <me@pantherman594.com>
2018-04-18 14:02:35 -0400

    Polybar transparency

--- scripts/.scripts/wallpaper.sh
+++ scripts/.scripts/wallpaper.sh
@@ -2,5 +2,14 @@
 
 cd /usr/share/backgrounds/pantherman594
 wget $(/home/pantherman594/.scripts/wallpaper.py) -O ./wallpaper.jpg
-wal -i ./wallpaper.jpg -c -n
+wal -i ./wallpaper.jpg -c -n -e
 feh --bg-fill --no-xinerama ./wallpaper.jpg
+
+cd /home/pantherman594/.cache/wal
+color=$(head colors -n 2 | tail -n 1)
+color="#50${color:1}"
+echo '[colors]' > poly
+echo "barbg = ${color}" >> poly
+
+cd -
+wal -i ./wallpaper.jpg -n
