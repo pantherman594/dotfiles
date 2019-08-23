f6d73f86dcf7a139a1fe281bb57d39a1b1219dc2
David Shen <me@pantherman594.com>
2019-08-22 21:51:35 -0400

    Add firefox chrome files

---
+++ mozilla/chrome/userChrome.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  document.querySelector('#PlacesToolbarItems > toolbarseparator').flex = "1";
+}, 1000);
+
+
+setTimeout(() => {
+  document.querySelector('#PlacesToolbarItems > toolbarseparator').flex = "1";
+}, 10000);
